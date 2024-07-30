dc17a139a0b04b2581e6791de9e29a1aa01caa24
BeSingularTeam <besingulardemo@gmail.com>
2024-07-30 23:39:00 +0530

    update

--- ai-travel-planner/src/components/custom/Header.jsx
+++ ai-travel-planner/src/components/custom/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div className="header bg-white text-black">
       <div className="header-content p-3 shadow-sm flex justify-between items-center px-5">
         <img src='/travelplanner.png' alt='Travel Logo' className="logo max-w-[12%] h-auto max-h-[30%] ml-1 bg-none rounded-lg -mt-4 no-shadow" />
-        <Button className="button bg-black text-white border-none py-2 px-4 text-lg cursor-pointer transition ease-in-out duration-300 mr-10 rounded-lg hover:bg-white hover:text-black -mt-6">Sign In</Button>
+        <Button className="button bg-black text-white border-none py-2 px-4 text-lg cursor-pointer transition ease-in-out duration-300 mr-10 rounded-lg hover:bg-white hover:text-black -mt-4">Sign In</Button>
       </div>
     </div>
   );
--- ai-travel-planner/src/components/custom/Hero.jsx
+++ ai-travel-planner/src/components/custom/Hero.jsx
@@ -1,18 +1,22 @@
 import { Button } from "../ui/Button";
+import {Link} from 'react-router-dom';
+// import React from 'react';
 
 function Hero() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white text-black gap-9">
-      <h1 className="font-extrabold text-4xl lg:text-5xl text-center mt-[-19rem]">
+      <h1 className="font-extrabold text-4xl lg:text-5xl text-center mt-[-22rem]">
         <span className="text-red-500">Discover Your Next Adventure with AI:</span>
         <br /> Personalized Itineraries at Your Fingertips
       </h1>
       <p className="text-sm lg:text-lg text-black text-center">
         Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
       </p>
-      <Button className="mt-8 bg-black text-white py-2 px-4 rounded hover:bg-white hover:text-black">
+      <Link to={'/create-trip'}>
+      <Button className="mt-8 bg-black text-white py-2 px-4 rounded hover:bg-white hover:text-black mt-[-8rem]">
         Get Started, It&#39;s Free
       </Button>
+      </Link>
     </div>
   );
 }
