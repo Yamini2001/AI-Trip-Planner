87445a5d2f2112ad03031dac3e51bbe06526194d
BeSingularTeam <besingulardemo@gmail.com>
2024-07-31 21:09:48 +0530

    update1

--- ai-travel-planner/src/create-trip/index.jsx
+++ ai-travel-planner/src/create-trip/index.jsx
@@ -1,11 +1,26 @@
-// import React from 'react'
+// import React from 'react';
+// import React from 'react';
+import GooglePlacesAutoComplete from 'react-google-places-autocomplete';
 
-function index() {
+function Index() {
+  const apiKey = import.meta.env.VITE_GOOGLE_PLACE_API_KEY;
+  
   return (
     <div>
-      <h2>CreateTrip</h2>
+      <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+        <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
+        <p className="mt-3 text-gray-500 text-xl">
+          Just provide basic information, and our trip planner will generate a customized itinerary based on your preferences.
+        </p>
+        <div className="mt-20">
+          <div>
+            <h2 className="text-xl my-3 font-medium">What is your destination of choice?</h2>
+            <GooglePlacesAutoComplete apiKey={apiKey} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default index
+export default Index;
