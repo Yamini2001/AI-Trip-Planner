3ccf69429fe0c0ac7fa12fe03b1d159b7bb2e16d
BeSingularTeam <besingulardemo@gmail.com>
2024-08-03 01:38:18 +0530

    commitUpdate

---
+++ ai-travel-planner/src/constants/options.jsx
@@ -0,0 +1,54 @@
+export const selectTravelList=[
+    {
+        id: 1,
+        title: 'Just Me',
+        desc:'A sole traveles in exploration',
+        icons:'✈️',
+        people:'1'
+    },
+    {
+        id: 2,
+        title: 'A Couple',
+        desc:'Two traveles in tandem',
+        icons:'👫',
+        people:'2 People'
+    },
+    {
+        id: 3,
+        title: 'Family',
+        desc:'A group of fun loving adv',
+        icons:'👨‍👩‍👧‍👦',
+        people:'3 to 5 People'
+    },
+    {
+        id: 4,
+        title: 'Friends',
+        desc:'A bunch of thrill-seekers',
+        icons:'👫👬👭👩‍👩‍👦',
+        people:'7 to 10 People'
+    },
+
+
+]
+export const SelectBudgetOptions=[
+    {
+        id: 1,
+        title: 'Cheap',
+        desc:'Stay conscious of costs',
+        icons:'💸',
+    },
+    {
+        id: 2,
+        title: 'Moderate',
+        desc:'Keep cost on the average side',
+        icons:'💵',
+    },
+    {
+        id: 1,
+        title: 'Luxury',
+        desc:'Dont worry about cost',
+        icons:'💎',
+    },
+
+]
+export const AI_PROMPT='Genrate Travel Plan for Location: {location}'
--- ai-travel-planner/src/create-trip/index.jsx
+++ ai-travel-planner/src/create-trip/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { SelectBudgetOptions, selectTravelList } from '../constants/options';
 
 function CreateTrip() {
   const [query, setQuery] = useState('');
@@ -43,7 +44,7 @@ function CreateTrip() {
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
-      <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
+      <h2 className="font-bold text-3xl">Tell us your travel preferences🏕️🌴</h2>
       <p className="mt-3 text-gray-500 text-xl">Just provide basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
       <div className="mt-20 flex flex-col gap-9">
         <div>
@@ -53,7 +54,7 @@ function CreateTrip() {
             value={query}
             onChange={handleInputChange}
             placeholder="Enter a location"
-            className="w-full md:w-1/2 lg:w-1/2 p-2 border border-gray-300 rounded"
+            className="w-full p-2 border border-gray-300 rounded"
           />
           {suggestions.length > 0 && (
             <ul className="border border-gray-300 rounded mt-2 max-w-md">
@@ -76,9 +77,36 @@ function CreateTrip() {
             value={days}
             onChange={handleDaysChange}
             placeholder="Ex. 3"
-            className="w-full md:w-1/2 lg:w-1/2 p-2 border border-gray-300 rounded"
+            className="w-full p-2 border border-gray-300 rounded"
           />
         </div>
+      </div>
+      <div>
+        <h2 className="text-xl my-3 font-medium">What is Your Budget?</h2>
+        <div className='grid grid-cols-3 gap-5 mt-5'>
+          {SelectBudgetOptions.map((item, index) => (
+            <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
+              <h2 className='text-4xl'>{item.icons}</h2>
+              <h2 className='font-bold text-lg'>{item.title}</h2>
+              <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h2 className="text-xl my-3 font-medium">Who do you plan on traveling with on your next adventure?</h2>
+        <div className='grid grid-cols-3 gap-5 mt-5'>
+          {selectTravelList.map((item, index) => (
+            <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
+              <h2 className='text-4xl'>{item.icons}</h2>
+              <h2 className='font-bold text-lg'>{item.title}</h2>
+              <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="my-10 justify-center">
+        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Generate Trip</button>
       </div>
     </div>
   );
