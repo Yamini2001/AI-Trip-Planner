9265b49d6abe2044fa03d6a06d31002d7639f7fd
BeSingularTeam <besingulardemo@gmail.com>
2024-08-20 19:14:30 +0530

    commit

--- ai-travel-planner/src/view-trip/components/InfoSection.jsx
+++ ai-travel-planner/src/view-trip/components/InfoSection.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+// import React from 'react';
 import placeHolder from '../placeholder.png';
+import { IoIosSend } from "react-icons/io";
 
 function InfoSection({ trip }) {
+  // Check if trip data is available
+  if (!trip || !trip.userSelection) {
+    return <div>Loading...</div>;
+  }
+  console.log(trip);
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <img 
         src={placeHolder}
         alt="Trip Placeholder" 
-        className="h-[450px] w-[1300px] object-cover mb-18" // Adjusted size and margin
+        className="h-[550px] w-full object-cover rounded-xl" // Adjusted size and margin
       />
-      <div className="flex flex-col items-center gap-4">
+      <div className='flex justify-between items-center'>
+      <div className="my-5 flex flex-col gap-2">
         <h2 className='font-bold text-2xl mb-4'>
-          {trip?.userSelection?.location?.label}
+          {trip?.userSelection?.location?.label || 'Location not available'}
         </h2>
-        <div className='flex gap-4'>
-          <h2 className="p-2 px-4 bg-gray-200 rounded-full text-gray-700">
-            {trip.userSelection?.noOfDays} Day
+        <div className='flex gap-5'>
+          <h2 className="p-2 px-4 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md">📆
+           {trip.userSelection?.noOfDays} Day
           </h2>
-          <h2 className="p-2 px-4 bg-gray-200 rounded-full text-gray-700">
-            {trip?.userSelection?.budget} Budget
+          <h2 className="p-2 px-4 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md">
+          💰 {trip.userSelection.budget ? `${trip.userSelection.budget} Budget` : 'Budget not specified'}
           </h2>
-          <h2 className="p-2 px-4 bg-gray-200 rounded-full text-gray-700">
-            No. of travelers: {trip.userSelection?.traveler}
+          <h2 className="p-2 px-4 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md">
+          🥂 {trip.userSelection.traveler ? `No. of travelers: ${trip.userSelection.traveler}` : 'Travelers not specified'}
           </h2>
         </div>
+        </div>
+        <button><IoIosSend /></button>
       </div>
     </div>
   );
