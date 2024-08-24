c0ee54739f0dbcec4d148a99eff98d79e82283f6
BeSingularTeam <besingulardemo@gmail.com>
2024-08-24 19:51:55 +0530

    update

--- ai-travel-planner/src/service/GlobalApi.jsx
+++ ai-travel-planner/src/service/GlobalApi.jsx
@@ -2,6 +2,7 @@ const BASE_URL = 'https://places.googleapis.com/v1/places:searchText'
 const config = {
     headers:{
         'Content-Type': 'application/json',
-        'X-Google-Api-Key' : 'pk.c51ba700c7aa3288f19b95fbaddbaeff'
+        'X-Google-Api-Key' : import.meta.env.VITE_GOOGLE_PLACE_API_KEY,
+        
     }
 }
